c17b8cae8b46c7ec9e054916036b65997ba0ab78
Sokcheatsrorng <sokcheat.srorng@gmail.com>
2024-04-09 12:17:51 +0700

    done

--- src/app/dashboard/update/[id]/page.tsx
+++ src/app/dashboard/update/[id]/page.tsx
@@ -18,7 +18,7 @@ export default async function page(props:any) {
   const data= await getData(props.id)
   console.log(data)
   return (
-	<div>
+	<>
       <UpdateProductForm 
         category={data.category.name}
         price={data.price}
@@ -29,58 +29,7 @@ export default async function page(props:any) {
         id={data.id}
         seller={data.seller}
       />
-	</div>
+	</>
   )
 }
 
-// import { baseApi } from '@/constants/baseApi';
-// import UpdateProductForm from '@/components/forms/UpdateForm';
-// import React, { useEffect, useState } from 'react';
-// import { Products } from '@/types/products';
-// type Props = {
-//   params: {
-//     id: number;
-//   };
-// };
-
-// const Page: React.FC<Props> = (props) => {
-//   const [productData, setProductData] = useState<Products | null>(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch(`${baseApi}products/${props.params.id}/`);
-//         if (!response.ok) {
-//           throw new Error('Failed to fetch data');
-//         }
-//         const data: Products = await response.json();
-//         setProductData(data);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, [props.params.id]);
-
-//   if (!productData) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div>
-//       <UpdateProductForm
-//         category={productData.category.name}
-//         price={productData.price}
-//         desc={productData.desc}
-//         name={productData.name}
-//         quantity={productData.quantity}
-//         image={productData.image}
-//         id={productData.id}
-//         seller={productData.seller}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Page;
